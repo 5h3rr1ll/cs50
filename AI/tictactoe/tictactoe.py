e148cf8100de017cf1dd4062d68b1c09e7eb2386
Anthony <anthony.sherrill@code.berlin>
2021-12-05 22:27:03 +0100

    Working Implementation + Tests

--- AI/tictactoe/tictactoe.py
+++ AI/tictactoe/tictactoe.py
@@ -9,7 +9,6 @@ X = "X"
 O = "O"
 EMPTY = None
 
-moves = set()
 
 def initial_state():
     """
@@ -28,14 +27,14 @@ def player(board):
     o = 0
     
     for row in board:
-        x += row.count("X")
-        o += row.count("O")
+        x += row.count(X)
+        o += row.count(O)
     
     if x == 0 and o == 0:
-        return "X"
+        return X
     elif x > o:
-        return "O"
-    return "X"
+        return O
+    return X
     
 
 
@@ -43,10 +42,14 @@ def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
+
+    moves = set()
+
     for idx, row in enumerate(board):
         for idy, move in enumerate(row):
             if move == EMPTY:
                 moves.add((idx, idy))
+
     return moves
 
 
@@ -54,9 +57,10 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    new_board = deepcopy(board)
     
-    if action in moves:
+    new_board = deepcopy(board)
+
+    if action in actions(board):
         new_board[action[0]][action[1]] = player(board)
         return new_board
 
@@ -65,7 +69,7 @@ def result(board, action):
 
 def winner(board):
     """
-    Returns the winner of the game, if there is one.
+    Returns the winner of the game, if there is one, otherweise None.
     """
     
     """Check if in one row are only one symbol like X or O = win state"""
@@ -75,37 +79,89 @@ def winner(board):
 
     """Check if in each row at the same poition is the same symbol like X or O = win state"""
     for i,row in enumerate(board):
-        print("BORAD: ", board)
-        if board[i][0] != EMPTY and board[i][0] == board[i+1][0] == board[i+2][0]:
-            return(board[i][0])
+        if board[0][i] != EMPTY and board[0][i] == board[1][i] == board[2][i]:
+            return(board[0][i])
 
-    for i,row in enumerate(board):
-        if board[i][1] != EMPTY and board[i][1] == board[i+1][1] == board[i+2][1]:
-            return(board[i][1])
+    """Check if diagonals are the same symbol like X or O = win state"""
+   
+    if board[0][0] != EMPTY and board[0][0] == board[1][1] == board[2][2]:
+        return(board[0][0])
 
-    for i,row in enumerate(board):
-        if board[i][2] != EMPTY and board[i][2] == board[i+1][2] == board[i+2][2]:
-            return(board[i][2])
-
-    """Check if in the middle (1) row at the poition 1 and in row 0 and 1 and position 0 and 2 is the same symbol like X or O = win state"""
+    elif board[0][2] != EMPTY and board[0][2] == board[1][1] == board[2][0]:
+        return(board[0][2])
+    else:
+        return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+
+    if winner(board) or len(actions(board)) == 0:
+        return True
+    else:
+        return False
 
 
 def utility(board):
     """
-    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    Returns 1 if X has won the game, -1 if O has won, 0 if no winner.
     """
-    raise NotImplementedError
+
+    if winner(board) == X:
+        return 1
+    elif winner(board) == O:
+        return -1
+    else:
+        return 0
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    if terminal(board):
+        return None
+    else:
+        if player(board) == X:
+            value, move = max_value(board)
+            return move
+        else:
+            value, move = min_value(board)
+            return move
+
+
+def max_value(board):
+        if terminal(board):
+            # If there is winner or no possivle moves left, return value of this end state and None.
+            # None indicates the end of the tree.
+            return utility(board), None
+
+        # -inf is the lowest possible value for a state
+        value = float('-inf')
+        move = None
+        # For each action, find the value of the next state and compare it to the current value.
+        for action in actions(board):
+            next_value, act = min_value(result(board, action))
+            if next_value == 1:
+                return next_value, action
+            elif next_value > value:
+                value = next_value
+                move = action
+        return value, move
+
+def min_value(board):
+        if terminal(board):
+            return utility(board), None
+
+        value = float('inf')
+        move = None
+        for action in actions(board):
+            next_value, act = max_value(result(board, action))
+            if value == -1:
+                    return value, move
+            elif next_value < value:
+                value = next_value
+                move = action
+        return value, move
--- AI/tictactoe/tictactoe_test.py
+++ AI/tictactoe/tictactoe_test.py
@@ -1,5 +1,7 @@
-from tictactoe import initial_state, player, result, actions, winner 
+from tictactoe import initial_state, player, result, actions, winner, terminal, utility 
 
+X = "X"
+O = "O"
 EMPTY = None
 board = initial_state()
 
@@ -8,49 +10,145 @@ def test_initial_board():
                      [EMPTY, EMPTY, EMPTY],
                      [EMPTY, EMPTY, EMPTY]]
 
-def test_player_x():
-    assert player(board) == "X"
+def test_next_player_x():
+    assert player(board) == X
 
-def test_player_y():
-    board = [["X", EMPTY, EMPTY],
+def test_next_player_y():
+    board = [[X, EMPTY, EMPTY],
               [EMPTY, EMPTY, EMPTY],
               [EMPTY, EMPTY, EMPTY]]
-    assert player(board) == "O"
+    assert player(board) == O
 
 def test_tictactoe_actions():
     assert actions(board) == {(0, 1), (1, 2), (0, 0), (2, 1), (2, 0), (1, 1), (2, 2), (1, 0), (0, 2)}
 
 def test_tictactoe_result():
-    assert result(board, (0, 0)) == [["X", EMPTY, EMPTY],
+    assert result(board, (0, 0)) == [[X, EMPTY, EMPTY],
                                      [EMPTY, EMPTY, EMPTY],
                                      [EMPTY, EMPTY, EMPTY]]
 
+def test_tictactoe_resul2():
+    assert result(board, (0, 2)) == [[EMPTY, EMPTY, X],
+                                     [EMPTY, EMPTY, EMPTY],
+                                     [EMPTY, EMPTY, EMPTY]]
+
+def test_tictactoe_resul3():
+    board = [
+        [EMPTY, EMPTY, X],
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY]
+        ]
+    assert result(board, (1, 1)) == [[EMPTY, EMPTY, X],
+                                     [EMPTY, O, EMPTY],
+                                     [EMPTY, EMPTY, EMPTY]]
+
 def test_winner_x_h_row():
-    assert winner([["X", "X", "X"],
+    assert winner([[X, X, X],
                    [EMPTY, EMPTY, EMPTY],
-                   [EMPTY, EMPTY, EMPTY]]) == "X"
+                   [EMPTY, EMPTY, EMPTY]]) == X
+
+def test_winner_x_h_row2():
+    assert winner([
+        [EMPTY, EMPTY, EMPTY],
+        [X, X, X],
+        [EMPTY, EMPTY, EMPTY]]) == X
+
+def test_winner_x_h_row3():
+    assert winner([
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY],
+        [X, X, X]
+        ]) == X
 
 def test_winner_o_h_row():
-    assert winner([["O", "O", "O"],
-                   [EMPTY, EMPTY, EMPTY],
-                   [EMPTY, EMPTY, EMPTY]]) == "O"
+    assert winner([
+        [O, O, O],
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY]
+        ]) == O
+
+def test_winner_o_h_row2():
+    assert winner([
+        [EMPTY, EMPTY, EMPTY],
+        [O, O, O],
+        [EMPTY, EMPTY, EMPTY]
+        ]) == O
+
+def test_winner_o_h_row3():
+    assert winner([
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY],
+        [O, O, O]
+        ]) == O
 
 def test_no_winner_h_row():
-    assert winner([["X", "O", "X"],
+    assert winner([[X, O, X],
                    [EMPTY, EMPTY, EMPTY],
                    [EMPTY, EMPTY, EMPTY]]) == None
 
 def test_winner_x_v_row1():
-    assert winner([["X", EMPTY, EMPTY],
-                   ["X", EMPTY, EMPTY],
-                   ["X", EMPTY, EMPTY]]) == "X"
+    assert winner([[X, EMPTY, EMPTY],
+                   [X, EMPTY, EMPTY],
+                   [X, EMPTY, EMPTY]]) == X
+
+def test_winner_o_v_row1():
+    assert winner([[O, EMPTY, EMPTY],
+                   [O, EMPTY, EMPTY],
+                   [O, EMPTY, EMPTY]]) == O
 
 def test_winner_x_v_row2():
-    assert winner([[EMPTY, "X", EMPTY],
-                   [EMPTY, "X", EMPTY],
-                   [EMPTY, "X", EMPTY]]) == "X"
+    assert winner([[EMPTY, X, EMPTY],
+                   [EMPTY, X, EMPTY],
+                   [EMPTY, X, EMPTY]]) == X
+
+def test_winner_o_v_row2():
+    assert winner([[EMPTY, O, EMPTY],
+                   [EMPTY, O, EMPTY],
+                   [EMPTY, O, EMPTY]]) == O
 
 def test_winner_x_v_row3():
-    assert winner([[EMPTY, EMPTY, "X"],
-                   [EMPTY, EMPTY, "X"],
-                   [EMPTY, EMPTY, "X"]]) == "X"
+    assert winner([[EMPTY, EMPTY, X],
+                   [EMPTY, EMPTY, X],
+                   [EMPTY, EMPTY, X]]) == X
+
+def test_winner_o_v_row3():
+    assert winner([[EMPTY, EMPTY, O],
+                   [EMPTY, EMPTY, O],
+                   [EMPTY, EMPTY, O]]) == O
+
+def test_winner_o_diagonal():
+    assert winner([[EMPTY, EMPTY, O],
+                   [EMPTY, O, EMPTY],
+                   [O, EMPTY, EMPTY]]) == O
+
+def test_winner_o_diagonal2():
+    assert winner([[O, EMPTY, EMPTY],
+                   [EMPTY, O, EMPTY],
+                   [EMPTY, EMPTY, O]]) == O
+
+def test_winner_x_diagonal():
+    assert winner([[X, EMPTY, EMPTY],
+                   [EMPTY, X, EMPTY],
+                   [EMPTY, EMPTY, X]]) == X
+
+def test_winner_o_diagonal2():
+    assert winner([[EMPTY, EMPTY, X],
+                   [EMPTY, X, EMPTY],
+                   [X, EMPTY, EMPTY]]) == X
+
+def test_terminal():
+    board = [
+        [O, X, O],
+        [O, X, X],
+        [X, X, O]
+        ]
+    assert terminal(board) == True
+
+def test_x_wins_wrong():
+    board = [
+        [X, O, X],
+        [EMPTY, O, X],
+        [O, X, EMPTY]
+        ]
+    print("Ya?", winner(board))
+    assert winner(board) == None
